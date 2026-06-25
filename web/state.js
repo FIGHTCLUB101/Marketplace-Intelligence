1bd24c3b2c5ddc0869d383df2291849de15fafd2
FIGHTCLUB101 <singhchetanind@gmail.com>
2026-06-25 20:00:13 +0530

    feat(web): shell, design tokens, AppState, map toggles

---
+++ web/state.js
@@ -0,0 +1,13 @@
+const AppState = {
+  selectedCity: null,
+  mapInstance: null,
+  markers: null,
+  initMap: null,
+  showLocality: null,
+  renderLeaderboard: null,
+  renderWhitespace: null,
+  renderGyms: null,
+  initMargin: null,
+  renderMethodology: null,
+};
+export default AppState;
